1bd338e69d59d88f295d7a182537131f3e06478f
Peter Mačinec <peter.macinec@gmail.com>
2019-04-10 12:50:37 +0200

    ADD: basic registration form

--- dochadzkovy_is/views.py
+++ dochadzkovy_is/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello World.')
+
+def registration(request):
+    return render(request, 'dochadzkovy_is/registration.html')
+
+def registrate(request):
+    print(request.POST['name'])
+    print(request.POST['email'])
+
+    # ws
+
+    # email zamestnancovi
+
+    # return redirect a hlaska
+    
+    return True
