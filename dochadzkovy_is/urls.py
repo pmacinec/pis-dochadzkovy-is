3e220c65d266113d65809aecd141cb14cba77c5a
Peter Mačinec <peter.macinec@gmail.com>
2019-04-16 08:30:16 +0200

    ADD: registration methods and templates

--- dochadzkovy_is/urls.py
+++ dochadzkovy_is/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('registration', views.registration, name='registration'),
-    path('registrate', views.registrate, name='registrate')
+    path('registrate', views.registrate, name='registrate'),
+    path('complete_registration/<int:employee_id>', views.complete_registration, name='complete_registration'),
+    path('employee/update', views.update_employee, name='update_employee')
 ]
