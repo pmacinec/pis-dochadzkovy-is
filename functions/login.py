74f1fc0c4ccb2cb13176524419c77faa26196646
Peter Mačinec <peter.macinec@gmail.com>
2019-04-23 13:12:55 +0200

    ADD: login form, login auxiliary functions

---
+++ functions/login.py
@@ -0,0 +1,30 @@
+from zeep import Client
+from zeep.xsd import SkipValue
+from functions import employee as e
+from functions import passwords
+
+def login(request, email, password):
+    
+    employee = e.get_by_email(email)
+
+    print(employee)
+    if not employee: return False
+
+    if not passwords.verify_password(employee[0].password, password): return False
+
+    request.session['employee_id'] = employee[0].id
+    
+    return True
+
+def logout(request):
+    if is_logged(request):
+        del request.session['employee_id']
+    
+def is_logged(request):
+    return 'employee_id' in request.session
+
+def get_logged_employee(request):
+    if is_logged(request):
+        return request.session['employee_id']
+    else:
+        return False
